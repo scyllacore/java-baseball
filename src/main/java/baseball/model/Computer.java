592564cadea42d5b4d73231d7990ece336f1f645
scyllacore <yms8795@gmail.com>
2022-11-29 16:00:51 +0900

    feat : 컴퓨터가 임의로 만들어낼 숫자를 저장하기 위한 객체 구현.
    세부 사항
    - 중복되지 않게 숫자를 3개 생성해내는 기능 구현.
    - 같은 위치에 같은 값이 있는지 확인해주는 기능 구현.
    - 다른 위치에 같은 값이 있는지 확인해주는 기능 구현.

---
+++ src/main/java/baseball/model/Computer.java
@@ -0,0 +1,42 @@
+package baseball.model;
+
+import baseball.constant.Constant;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+
+    private final Numbers numbers;
+
+    public Computer() {
+        this.numbers = new Numbers(createRandomThreeNumbers());
+        //System.out.println(numbers.getNumbers());
+    }
+
+    private List<Integer> createRandomThreeNumbers() {
+        List<Integer> numbers = new ArrayList<>();
+
+        for (; numbers.size() < Constant.NUMBERS_SIZE; ) {
+            int number = Randoms.pickNumberInRange(Constant.MIN_NUMBER, Constant.MAX_NUMBER);
+            if (isNoDuplication(numbers, number)) {
+                numbers.add(number);
+            }
+        }
+        return numbers;
+    }
+
+    private boolean isNoDuplication(List<Integer> numbers, int number) {
+        return !numbers.contains(number);
+    }
+
+    public boolean isSamePositionSameValue(int number, int position) {
+        return numbers.getNumbers().get(position) == number;
+    }
+
+    public boolean isDifferentPositionSameValue(int number, int position) {
+        return numbers.getNumbers().get(position) != number
+                && numbers.getNumbers().contains(number);
+    }
+}
