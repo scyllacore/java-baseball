91e7d8a1329f85745034a96412094af25627dd2b
scyllacore <yms8795@gmail.com>
2022-11-29 15:51:43 +0900

    feat : 필요한 상수들을 저장하기 위한 클래스 구현.

---
+++ src/main/java/baseball/constant/Constant.java
@@ -0,0 +1,13 @@
+package baseball.constant;
+
+public class Constant {
+    public static final int NUMBERS_SIZE = 3;
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 9;
+
+    public static final int COUNT_SUCCESS = 1;
+    public static final int COUNT_FAIL = 0;
+    public static final int ANSWER_COUNT = 3;
+
+    public static final String RETRY = "1";
+}
